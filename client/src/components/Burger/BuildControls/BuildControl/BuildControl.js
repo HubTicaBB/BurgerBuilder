ca99879d041801e38acd4b70334990379991541e
Tijana <tijana.ilic87@protonmail.com>
2020-08-03 00:17:27 +0200

    Create BuildControls +/-

---
+++ client/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../../../bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+
+const BuildControl = props => (
+  <div className="btn-group mb-2">
+    <button className='btn btn-success'>
+      <FontAwesomeIcon icon={ faMinus } />
+    </button>  
+    <button className='btn btn-secondary' style={{ minWidth: '100px' }}>
+      { props.label }
+    </button>  
+    <button className='btn btn-danger'>
+      <FontAwesomeIcon icon={ faPlus } />
+    </button>
+  </div>  
+);
+
+export default BuildControl;
